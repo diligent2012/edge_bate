aecb1038255eac41392566a11e34fa2e89d36b25
ChangAiQing <changaiqing@vip.163.com>
2017-12-21 00:14:51 +0800

    btc

---
+++ eos/buy.py
@@ -0,0 +1,140 @@
+#!/usr/bin/python
+#coding:utf-8
+import sys  
+import sqlite3
+import requests
+import json
+from bs4 import BeautifulSoup
+import re
+from datetime import datetime
+from datetime import timedelta
+import MySQLdb
+import time
+
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+
+
+def crawl_enter_list():
+    result={}
+    header = {
+        'Accept-Language': 'zh-CN,zh;q=0.9',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36',
+        'Host': 'otcbtc.com',
+    }
+    response = requests.get('https://otcbtc.com/buy_offers?currency=eos&fiat_currency=cny&payment_type=all', headers=header, verify=False)
+    if response.status_code != 200:
+        return result
+
+    try:
+        soup = BeautifulSoup(response.text, "html.parser")
+        info_list = soup.find_all('ul',class_="list-content")
+        date = time.strftime('%Y-%m-%d',time.localtime(time.time()))
+        crawl_date = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+        for key,item in enumerate(info_list):
+            #print item
+            price = item.find('li',class_="price")
+            price.span.extract()
+            price.span.extract()
+            price_buy = price.get_text().strip().replace(',','')
+
+
+            minimum_amount = item.find('li',class_="minimum-amount")
+            minimum_amount.span.extract()
+            minimum_amount.span.extract()
+            min_single =  minimum_amount.get_text().strip().split('-')[0].strip().replace(',','')
+            max_single =  minimum_amount.get_text().strip().split('-')[1].strip().replace(',','')
+
+            buy_link = item.find('li',class_="buy-button")
+            sell_id = buy_link.a['href'].split('?')[0].split('/')[2]
+
+            is_crawl = find_buy_refer(sell_id)
+            print is_crawl
+            if(is_crawl):
+                print min_single, max_single, price_buy
+                insert_data(price_buy, min_single, max_single, sell_id, date, crawl_date)
+                insert_buy_refer(sell_id, date, crawl_date)
+
+
+    except Exception, e:
+        print e.message
+
+
+def insert_data(price, min_single, max_single, sell_id, date, crawl_date):
+    try:
+        conn= MySQLdb.connect(
+            host='127.0.0.1',
+            port = 3306,
+            user='omni_btc',
+            passwd='!omni123456btcMysql.pro',
+            db ='z_omni_btc',
+        )
+        cur = conn.cursor()
+        cur.execute('set names utf8') #charset set code. it is not nessary now
+        sql = "INSERT INTO `t_btc_buy` (`price`, `min_single`, `max_single`, `sell_id`, `date`, `crawl_date`) VALUES ('%s', '%s', '%s', '%s', '%s', '%s')" % (price, min_single, max_single, sell_id, date, crawl_date)
+        #sql = "INSERT INTO `ecs_t_marathon` (`name`, `start_run_time`) VALUES ('%s', '%s')" % (name, start_run_time)
+        cur.execute(sql)
+        conn.commit()
+        cur.close()
+        conn.close()
+    except Exception, e:
+        print e.message
+
+
+def insert_buy_refer(sell_id, date, crawl_date):
+    try:
+        conn= MySQLdb.connect(
+            host='127.0.0.1',
+            port = 3306,
+            user='omni_btc',
+            passwd='!omni123456btcMysql.pro',
+            db ='z_omni_btc',
+        )
+        cur = conn.cursor()
+        cur.execute('set names utf8') #charset set code. it is not nessary now
+        sql = "INSERT INTO `t_btc_buy_refer` (`sell_id`, `date`, `crawl_date`) VALUES ('%s', '%s', '%s')" % (sell_id, date, crawl_date)
+        #sql = "INSERT INTO `ecs_t_marathon` (`name`, `start_run_time`) VALUES ('%s', '%s')" % (name, start_run_time)
+        cur.execute(sql)
+        conn.commit()
+        cur.close()
+        conn.close()
+    except Exception, e:
+        print e.message
+
+def find_buy_refer(sell_id):
+    try:
+        conn= MySQLdb.connect(
+            host='127.0.0.1',
+            port = 3306,
+            user='omni_btc',
+            passwd='!omni123456btcMysql.pro',
+            db ='z_omni_btc',
+        )
+        cur = conn.cursor()
+        cur.execute('set names utf8') #charset set code. it is not nessary now
+        sql = "SELECT * FROM `t_btc_buy_refer`  WHERE sell_id = '%s'" % (sell_id)
+        #sql = "INSERT INTO `ecs_t_marathon` (`name`, `start_run_time`) VALUES ('%s', '%s')" % (name, start_run_time)
+        cur.execute(sql)
+        conn.commit()
+        result = cur.fetchall()
+        if result:
+            return False
+        else:
+            return True
+        
+        cur.close()
+        conn.close()
+    except Exception, e:
+        print e.message
+    return True
+
+def crawl_start():
+    print "start : " + time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+    crawl_enter_list()
+
+def main():
+    crawl_start();
+
+if __name__ == '__main__':
+    main()
