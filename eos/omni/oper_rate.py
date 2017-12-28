b558f2f7ed346f0457fa92e467c6117d1880be5d
ChangAiQing <changaiqing@vip.163.com>
2017-12-28 13:01:06 +0800

    btc

--- eos/omni/oper_rate.py
+++ eos/omni/oper_rate.py
@@ -64,7 +64,7 @@ user_agent_list = ["Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/
                       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11"
                       ]
 
-def crawl_enter_sell():
+def crawl_enter_sell_eos():
     result={}
     header = {
         'Accept-Language': 'zh-CN,zh;q=0.9',
@@ -99,7 +99,6 @@ def crawl_enter_sell():
             
             buy_link = item.find('li',class_="buy-button")
             buy_id = buy_link.a['href'].split('?')[0].split('/')[2]
-            print price_sell
             return price_sell
             break
 
@@ -108,6 +107,53 @@ def crawl_enter_sell():
         print e.message
 
     return 0
+
+
+
+def crawl_enter_sell_dew():
+    result={}
+    header = {
+        'Accept-Language': 'zh-CN,zh;q=0.9',
+        #'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36',
+        'Host': 'otcbtc.com',
+    }
+    header['User-Agent'] = random.sample(user_agent_list, 1)
+
+    response = requests.get('https://otcbtc.com/sell_offers?currency=dew&fiat_currency=cny&payment_type=all', headers=header, verify=False)
+    if response.status_code != 200:
+        return result
+
+    try:
+        soup = BeautifulSoup(response.text, "html.parser")
+        info_list = soup.find_all('ul',class_="list-content")
+        date = time.strftime('%Y-%m-%d',time.localtime(time.time()))
+        crawl_date = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+        for key,item in enumerate(info_list):
+            #print item
+            price = item.find('li',class_="price")
+            price.span.extract()
+            price.span.extract()
+            price_sell = price.get_text().strip().replace(',','')
+
+
+            minimum_amount = item.find('li',class_="minimum-amount")
+            minimum_amount.span.extract()
+            minimum_amount.span.extract()
+            min_amount =  minimum_amount.get_text().strip().split('-')[0].strip().replace(',','')
+            max_amount =  minimum_amount.get_text().strip().split('-')[1].strip().replace(',','')
+            #print min_amount, max_amount
+            
+            buy_link = item.find('li',class_="buy-button")
+            buy_id = buy_link.a['href'].split('?')[0].split('/')[2]
+            return price_sell
+            break
+
+
+    except Exception, e:
+        print e.message
+
+    return 0
+
 
 
 def insert_send_refer(flag, date, crawl_date):
@@ -144,7 +190,6 @@ def find_send_refer(flag):
         cur.execute('set names utf8') #charset set code. it is not nessary now
         sql = "SELECT count(*) as send_count FROM `t_btc_send_refer`  WHERE flag = '%s' " % (flag)
         #sql = "INSERT INTO `ecs_t_marathon` (`name`, `start_run_time`) VALUES ('%s', '%s')" % (name, start_run_time)
-        print sql
         cur.execute(sql)
         conn.commit()
         result = cur.fetchone()
@@ -254,13 +299,21 @@ def start_monitor():
     rate_list = [1.03,1.02]
     for item_buy in order_buy:
         price = item_buy[3] # 购买价格
-
         buy_quantity = item_buy[2] # 购买数量
         buy_id = item_buy[0]# 购买表主键
+        currency = item_buy[-1]# 购买币种
 
-        # 当前售卖最低价格
-        curr_min_sell_price =  crawl_enter_sell() 
-        curr_min_sell_price = float(curr_min_sell_price);
+
+        # 当前售卖最低价格 EOS
+        curr_min_sell_eos_price =  crawl_enter_sell_eos() 
+        curr_min_sell_eos_price  = float(curr_min_sell_eos_price);
+
+        # 当前售卖最低价格 DEW
+        curr_min_sell_dew_price =  crawl_enter_sell_dew() 
+        curr_min_sell_dew_price  = float(curr_min_sell_dew_price);
+
+        #print currency, curr_min_sell_eos_price, curr_min_sell_dew_price
+            
 
         for rate in rate_list:
 
@@ -270,16 +323,31 @@ def start_monitor():
             rate_price = sell_price - price
             rate_total_price = rate_price * float(buy_quantity)
 
-            print  buy_id, price, rate
+            
             is_cal = find_rate_is_cal(buy_id, rate)
             if(is_cal):
                 insert_buy_rate(buy_id, sell_price, rate, rate_price, rate_total_price, crawl_date)
 
-            if (curr_min_sell_price >= sell_price):
-                key_flag = str(curr_min_sell_price) + str(price) + str(rate_price)
+
+            if((currency == "EOS" and curr_min_sell_eos_price >= sell_price) 
+                    or (currency == "DEW" and curr_min_sell_dew_price >= sell_price)):
+
+                curr_sell_price = 0
+
+                if (currency == "EOS"):
+                    curr_sell_price = curr_min_sell_eos_price
+
+                if (currency == "DEW"):
+                    curr_sell_price = curr_min_sell_dew_price
+                
+            #if (curr_min_sell_price >= sell_price):
+                
+                print  buy_id, price, rate, currency, curr_sell_price
+
+                key_flag = str(curr_sell_price) + str(price) + str(rate_price)
                 is_send = find_send_refer(key_flag)
                 if(is_send):
-                    content = "\n当前售卖价格: " + str(curr_min_sell_price) + ";\n" + "当初购买价格: " + str(price) + ";\n" + "建议出售价格: " + str(sell_price) + ";\n" + " 利润: " + str(rate_price) + ";\n" + " 利润率: " + str(rate) + ";\n" + " 对应产品ID为: " + str(buy_id)
+                    content = "\n当前售卖价格: " + str(curr_sell_price) + ";\n" + "当初购买价格: " + str(price) + ";\n" + "建议出售价格: " + str(curr_sell_price) + ";\n" + " 利润: " + str(rate_price) + ";\n" + " 利润率: " + str(rate) + ";\n" + " 对应产品ID为: " + str(buy_id)
                     print content
                     send_mail(content)
                     insert_send_refer(key_flag, date, crawl_date)
