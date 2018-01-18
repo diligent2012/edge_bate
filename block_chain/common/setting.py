dd873061c678728343112274cbbc4e8b1f4687b1
ChangAiQing <changaiqing@vip.163.com>
2018-01-19 01:00:44 +0800

    价格颠倒

--- block_chain/common/setting.py
+++ block_chain/common/setting.py
@@ -64,12 +64,31 @@ def get_account_list():
             "start_auto_date":"2018-01-17 00:00:00",
             "api_key":"HYjjZgAEP7b68Egex3IoWGe9C9K3hUvpW0lhRKKJ3prnzAyOdaoaMvGudJgeX7oC",
             "api_secret":"I4SqDlfkVk6Y01UgXdwdG9720Pdc859xa4ShRKdo35GvVOVrvB1ceURx5zW8XiHH",
-            'qty': 3,
+            'qty': 200,
             "allow_symbol": [
                 {
                     "symbol":"EOSETH" # 以太坊
                 }
                
+            ]
+        }
+    ]
+    return ACCOUNT_LIST
+
+# 所有账户
+def get_windfall_account_list():
+
+    ACCOUNT_LIST = [
+        {
+            "account":"changaiqing",
+            "start_auto_date":"2018-01-17 00:00:00",
+            "api_key":"ugXds1RBgjoc6n5WPtRyH2n9ahXw56bfLfFJJEW7cHqK2KzMZ3D1ZjhwhGBp455U",
+            "api_secret":"zQM8ci1whr3gych0WSQYA591zf5plZQlYBHHeDOJdpIQ1w2i9ug4v8pdqX57BvzU",
+            'qty': 3,
+            "allow_symbol": [
+                {
+                    "symbol":"EOSBTC" # EOS
+                }
             ]
         }
     ]
