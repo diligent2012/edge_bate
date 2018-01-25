370983ea300d20ba15979631cfb663c6933946bb
ChangAiQing <changaiqing@vip.163.com>
2018-01-25 13:08:01 +0800

    增加重新买入或卖出

--- block_chain/common/setting.py
+++ block_chain/common/setting.py
@@ -104,7 +104,7 @@ def get_same_account_list():
             "start_auto_date":"2018-01-17 00:00:00",
             "api_key":"ugXds1RBgjoc6n5WPtRyH2n9ahXw56bfLfFJJEW7cHqK2KzMZ3D1ZjhwhGBp455U",
             "api_secret":"zQM8ci1whr3gych0WSQYA591zf5plZQlYBHHeDOJdpIQ1w2i9ug4v8pdqX57BvzU",
-            'qty': 10,
+            'qty': 20,
             "allow_symbol": [
                 {
                     "symbol":"EOSBTC" # EOS
